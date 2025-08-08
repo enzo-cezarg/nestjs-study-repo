8fd105d63b5309063ac28037cd37720d4d61e935
Enzo Garcia <enzoocezargarcia@gmail.com>
2025-08-08 13:23:15 -0300

    first commit: dto created

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Course } from './courses.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {id: 1, name: 'NestJS', description: 'Curso sobre fundamentos do Framework NestJS', tags: ['node.js', 'nestjs', 'javascript', 'typescript']}
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    const course = this.courses.find(course => course.id === id);
+    if (!course) {
+      throw new NotFoundException(`Course ID ${id} not found.`)
+    }
+    return course;
+  }
+
+  create(createCourseDTO: any) {
+    return this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const existingCourse = this.findOne(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex(course => course.id === id)
+      this.courses[index] = {id, ...updateCourseDTO}
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex(course => course.id === id)
+    if (index >= 0) {
+      this.courses.splice(index, 1);
+    }
+  }
+
+}
